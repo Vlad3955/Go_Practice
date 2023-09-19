58379df04c4dcfe9f0ce48511c8fd32a33e33684
Vlad3955 <vladreut28@gmail.com>
2023-09-19 18:51:06 +0300

    some mod to Data_Dtructures/Linked_List/Go_Practice

--- Data_Structures/Linked_List/main.go
+++ Data_Structures/Linked_List/main.go
@@ -15,28 +15,68 @@ type LinkedList struct {
 	elementCount int
 }
 
-func (ls LinkedList) Init(v int, n *Node) (int, *Node) {
-	ls.head.value = v
-	ls.head.next = n
-	return v, n
-}
-
-func (ls LinkedList) Size() int {
+func (ls *LinkedList) Size() int {
 	return ls.elementCount
 }
 
-func (ls LinkedList) Empty() bool {
+func (ls *LinkedList) Empty() bool {
 	return ls.elementCount == 0
 }
 
-func (ls LinkedList) pushFront(value int) {
-	ls.head.value, ls.head.next = ls.Init(value, ls.head.next)
+func (ls *LinkedList) pushFront(value int) {
+	newNode := &Node{value: value}
+
+	if ls.head == nil {
+		ls.head = newNode
+		ls.elementCount++
+		return
+	}
+
+	current := ls.head
+	for current.next != nil {
+		current = current.next
+	}
+
+	current.next = newNode
 	ls.elementCount++
 }
 
+func (ls *LinkedList) popFront() {
+	if ls.head == nil {
+		return
+	}
 
+	ls.head = ls.head.next
+	ls.elementCount--
+}
 
+func (ls *LinkedList) printList() []int {
+	var elems []int
+	for i := ls.head; i != nil; i = i.next {
+		elems = append(elems, i.value)
+	}
+	return elems
+}
 
 func main() {
+	ls := &LinkedList{}
+	ls.pushFront(11)
+	ls.pushFront(54)
+	ls.pushFront(33)
+	fmt.Println("List:", ls.printList())
+	fmt.Println("Size:", ls.Size())
+	printList(ls)
+	ls.popFront()
+	fmt.Println("List:", ls.printList())
+	fmt.Println("Size:", ls.Size())
+	printList(ls)
+}
+
+func printList(l *LinkedList) {
+	curr := l.head
+	for curr != nil {
+		fmt.Printf("%d ", curr.value)
+		curr = curr.next
+	}
 	fmt.Println()
 }
